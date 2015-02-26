428b35d97a64aae5f323c3e69655b5b2b6bb04c7
Eduardo Aros <e.arosb@gmail.com>
2015-02-26 19:28:56 -0300

    opción de reestablecer contraseña comentada

--- app_core/app/views/user/login.blade.php
+++ app_core/app/views/user/login.blade.php
@@ -6,7 +6,7 @@
 
 <style type="text/css">
     .well.login-box {
-        width: 300px;
+        width: 400px;
         margin: 0 auto;
     }
 
@@ -25,27 +25,27 @@
 @section('content')
 
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-xs-12 col-md-12">
             <div class="well login-box">
                 {{ Form::open(array('url' => 'login')) }}
                 <legend>{{ trans('form.signin'); }}</legend>
                 @if($errors->has('login'))
                     <div class="alert alert-dismissable alert-danger">
-                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        {{--<button type="button" class="close" data-dismiss="alert">×</button>--}}
                         <p><strong>{{ $errors->first('login', ':message') }}</strong></p>
                     </div>
                 @endif
                 <div class="form-group">
                     {{ Form::label('username', trans('form.username')) }}
-                    {{ Form::text('username', '', array('class' => 'form-control', 'placeholder' => 'Ingrese usuario', 'required' => 'required')) }}
+                    {{ Form::text('username', '', array('class' => 'form-control', 'placeholder' => 'Ingrese Usuario', 'required' => 'required')) }}
                 </div>
                 <div class="form-group">
                     {{ Form::label('password', trans('form.password')) }}
-                    {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Ingrese contraseña', 'required' => 'required')) }}
+                    {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Ingrese Contraseña', 'required' => 'required')) }}
                 </div>
-                <div class="form-group">
+                {{--<div class="form-group">
                     {{ HTML::link('/', trans('form.password_lost')) }}
-                </div>
+                </div>--}}
                 <div class="form-group text-center">
                     {{ Form::submit(trans('form.signin'), array('class'=>'btn btn-success btn-login-submit')) }}
                 </div>
