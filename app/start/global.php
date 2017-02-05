953517b80625d0b05fe91fe882a3955f49b75e0d
Eduardo Aros <e.arosb@gmail.com>
2017-02-05 01:53:29 -0300

    logs files

--- app/start/global.php
+++ app/start/global.php
@@ -35,8 +35,7 @@ ClassLoader::addDirectories(array(
 //Log::useFiles(storage_path().'/logs/laravel.log');
 $logFile = 'laravel.log';
 
-//Log::useDailyFiles(storage_path().'/logs/'.$logFile);
-Log::useDailyFiles(__DIR__.'/../../../logs/'.$logFile);
+Log::useDailyFiles(storage_path().'/logs/'.$logFile);
 
 /*
 |--------------------------------------------------------------------------
