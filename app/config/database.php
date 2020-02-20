893324f541d38fcd91f54417b15a066a1917532e
Eduardo Aros <e.arosb@gmail.com>
2020-02-19 21:10:36 -0300

    Docker deployment

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
         'mysql'  => array(
             'driver'    => 'mysql',
-            'host'      => 'localhost',
+            'host'      => 'db',
 			'database'  => 'iipzs',
 			'username'  => 'iipzs',
 			'password'  => 'Y8yJokCv',
